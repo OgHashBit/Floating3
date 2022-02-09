49ddc8e0b5684460216833f315c573e38297ccd8
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-09 13:56:52 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+// Components
+import Nav from "components/Nav/Nav";
+import Home from "components/Home/Home";
+import Contact from "components/Contact/Contact
+// Animation
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { Menu } from "components/Menu/Menu";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    Aos.init({ duration: 2000, offset: 10 });
+  }, []);
+
+  return (
+    <div className="app-container">
+      <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+      <Contact 
+/>
+    </div>
+  );
+}
+
+export default App;
